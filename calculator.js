6f17293a700894beeca6b5abe050763953778e56
Joshua Lee <jdl93@Joshuas-MacBook-Air.local>
2017-05-18 17:49:14 -1000

    refactored per Raymond's request

--- calculator.js
+++ calculator.js
@@ -9,7 +9,7 @@
 function calculatorModule(x){
   var memory = 0;
   var total = 0;
-  var calculator = {
+  var calculator = {};
 
   /**
    * sets the `total` to the number passed in
@@ -17,96 +17,96 @@ function calculatorModule(x){
    * @return { Number }    current total
    */
 
-  load: function load(x){
+  function load(x){
     total = x;
     if(typeof x === 'number'){
       return total;
     } else {
       throw new Error('you suck');
-    }
-  },
+  }
+}
 
   /**
    * Return the value of `total`
    * @return { Number }
    */
-  getTotal: function getTotal(){
+  function getTotal(){
     return total;
-  },
+  }
 
   /**
    * Sums the value passed in with `total`
    * @param { Number } x
    */
 
-  add: function add(x){
+  function add(x){
     if(typeof x === 'number'){
       return total += x;
     } else {
       throw new Error('you suck');
     }
-  },
+  }
 
   /**
    * Subtracts the value passed in from `total`
    * @param  { Number } x
    */
 
-   subtract: function subtract(x){
+   function subtract(x){
     if(typeof x === 'number'){
       return total -= x;
     } else {
       throw new Error('you suck');
     }
-   },
+   }
 
   /**
    * Multiplies the value by `total`
    * @param  { Number } x
    */
 
-  multiply: function multiply(x){
+  function multiply(x){
     if(typeof x === 'number'){
       return total *= x;
     } else {
       throw new Error('you suck');
     }
-  },
+  }
 
   /**
    * Divides the value passing in by `total`
    * @param  { Number } x
    */
-  divide: function divide(x){
+  function divide(x){
     if(typeof x === 'number'){
       return total /= x;
     } else {
       throw new Error('you suck');
     }
-  },
+  }
 
   /**
    * Return the value stored at `memory`
    * @return { Number }
    */
 
-  recallMemory: function recallMemory(){
+  function recallMemory(){
     return memory;
-  },
+  }
 
   /**
    * Stores the value of `total` to `memory`
    */
 
-  saveMemory: function saveMemory(){
+  function saveMemory(){
     memory = total;
-  },
+  }
 
   /**
    * Clear the value stored at `memory`
    */
 
-  clearMemory: function clearMemory(){
+  function clearMemory(){
     memory = 0;
   }
 
@@ -114,8 +114,8 @@ function calculatorModule(x){
    * Validation
    */
 
-  };
- return {
+
+  return {
     load: load,
     getTotal: getTotal,
     add: add,
@@ -125,8 +125,6 @@ function calculatorModule(x){
     recallMemory: recallMemory,
     saveMemory: saveMemory,
     clearMemory: clearMemory
-  };
- }
-
- calculatorModule();
+    };
+}
 
